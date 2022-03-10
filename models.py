49bc41d4ae611b090e89b0401b08162116515b25
Vikramaditya24 <81467681+Vikramaditya24@users.noreply.github.com>
2022-03-10 18:46:01 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-import email
-from django.db import models
-
-# Create your models here.
-class quiz(models.Model):
-    name=models.CharField(max_length=100,verbose_name="Name")
-    email=models.EmailField(verbose_name="Email")
-    result=models.CharField(max_length=20,verbose_name="Result")
